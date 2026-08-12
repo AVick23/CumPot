3ac0ad579ed26ad1b3e76cd213ba9e390622b1a4
AVick <vikulov2811@gmail.com>
2026-08-12 20:54:41 +0300

    V 0.8.0

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -31,6 +31,7 @@ def register_handlers(app: Application):
     register_employee_states(states)
     register_admin_states(states)
 
+    # handlers/__init__.py
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start_router)],
         states=states,
@@ -38,5 +39,6 @@ def register_handlers(app: Application):
         per_user=True,
         per_chat=False,
         allow_reentry=True,
+        state_key="employee_state",   # ← добавить эту строку
     )
     app.add_handler(conv_handler)
