393fe8dd24a563e50887d95e263fef0ff7ed0b6e
AVick <vikulov2811@gmail.com>
2026-08-12 20:13:35 +0300

    V 0.6.8

---
+++ handlers/admin/menu/constants.py
@@ -0,0 +1,12 @@
+# Состояния для меню
+ADMIN_MAIN = 100
+ADMIN_SHIFTS = 101
+
+# Callback data
+CB_HOME = "home"
+CB_SHIFTS = "shifts"
+CB_CALENDAR = "calendar"
+CB_EDIT = "edit"
+
+# UI
+MSG_LIMIT = 3800
